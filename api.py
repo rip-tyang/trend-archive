58b1aa21f9fcd355a6d906c0aa6a66dd7eaa930f
Thomas (Hongjian) Yang <jscnm1993@gmail.com>
2023-12-24 22:58:24 -0700

    fix Bilibili adding security check to request header

--- api.py
+++ api.py
@@ -36,7 +36,20 @@ class BilibiliApi(object):
 
     @classmethod
     def _get(cls, url: str) -> Dict[str, Any]:
-        res = requests.get(url)
+        res = requests.get(url, headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cache-Control': 'max-age=0',
+            'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"macOS"',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1',
+        })
         print(f'getting {url}')
         if res.status_code != 200:
             raise ValueError(f'Status code: {res.status_code}\n Content: {res.text}')
