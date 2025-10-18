8b4f047aab50887e2e10b2d476d87b25e04d9def
Thomas (Hongjian) Yang <jscnm1993@gmail.com>
2025-10-18 16:33:10 -0400

    add github

--- api.py
+++ api.py
@@ -1,41 +1,20 @@
 from typing import Dict, List, Any
 
+from bs4 import BeautifulSoup
+from os import path
+from datetime import date
+from time import sleep
 import requests
 import json
+import re
 
-class BilibiliApi(object):
-    BASE_URL = 'https://api.bilibili.com'
-    RAW_DATA_T = List[Dict[str, Any]]
+from writer import write_raw_data, write_md
 
-    @classmethod
-    def get_highest_ranked(cls) -> RAW_DATA_T:
-        url = f'{cls.BASE_URL}/x/web-interface/ranking'
-        return cls._get_data_list(url)
+class BaseApi(object):
+    BASE_PATH = './archive'
 
     @classmethod
-    def get_most_popular(cls) -> RAW_DATA_T:
-        url = f'{cls.BASE_URL}/x/web-interface/popular'
-        return cls._get_data_list(url)
-
-    @classmethod
-    def get_tag(cls, aid) -> RAW_DATA_T:
-        url = f'{cls.BASE_URL}/x/tag/archive/tags?aid={aid}'
-        return cls._get_data(url)
-
-    @classmethod
-    def _get_data(cls, url: str) -> RAW_DATA_T:
-        json_data = cls._get(url)
-        raw_data_list = json_data['data']
-        return raw_data_list
-
-    @classmethod
-    def _get_data_list(cls, url: str) -> RAW_DATA_T:
-        json_data = cls._get(url)
-        raw_data_list = json_data['data']['list']
-        return raw_data_list
-
-    @classmethod
-    def _get(cls, url: str) -> Dict[str, Any]:
+    def _get(cls, url: str) -> str:
         res = requests.get(url, headers={
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
             'Accept-Encoding': 'gzip, deflate',
@@ -53,9 +32,220 @@ class BilibiliApi(object):
         print(f'getting {url}')
         if res.status_code != 200:
             raise ValueError(f'Status code: {res.status_code}\n Content: {res.text}')
+        return res.text
+
+    @classmethod
+    def _get_json(cls, url: str) -> Dict[str, Any]:
         try:
-            return json.loads(res.text)
+            result = cls._get(url)
+            return json.loads(result)
         except json.decoder.JSONDecodeError as e:
             print(e)
-            print(res.text)
+            print(result)
 
+    @classmethod
+    def _get_parsed_html(cls, url: str) -> BeautifulSoup:
+        result = cls._get(url)
+        return BeautifulSoup(result, 'html.parser')
+
+    @classmethod
+    def archive_for_today(cls) -> None:
+        raise NotImplementedError
+class BilibiliApi(BaseApi):
+    LOC = 'Bilibili'
+    NAP_TIME = .5
+    BASE_URL = 'https://api.bilibili.com'
+    RAW_DATA_T = List[Dict[str, Any]]
+
+    @classmethod
+    def _get_highest_ranked(cls) -> RAW_DATA_T:
+        url = f'{cls.BASE_URL}/x/web-interface/ranking'
+        return cls._get_data_list(url)
+
+    @classmethod
+    def _get_most_popular(cls) -> RAW_DATA_T:
+        url = f'{cls.BASE_URL}/x/web-interface/popular'
+        return cls._get_data_list(url)
+
+    @classmethod
+    def _get_tag(cls, aid) -> RAW_DATA_T:
+        url = f'{cls.BASE_URL}/x/tag/archive/tags?aid={aid}'
+        return cls._get_data(url)
+
+    @classmethod
+    def _get_data(cls, url: str) -> RAW_DATA_T:
+        json_data = cls._get_json(url)
+        raw_data_list = json_data['data']
+        return raw_data_list
+
+    @classmethod
+    def _get_data_list(cls, url: str) -> RAW_DATA_T:
+        json_data = cls._get_json(url)
+        raw_data_list = json_data['data']['list']
+        return raw_data_list
+
+    @classmethod
+    def _get_tags(cls, aids: List[str]) -> None:
+        all_tags = {}
+        for aid in aids:
+            sleep(cls.NAP_TIME)
+            tag_list = BilibiliApi._get_tag(aid)
+            for tag in tag_list:
+                if tag['tag_id'] in all_tags:
+                    all_tags[tag['tag_id']]['day_count'] += 1
+                else:
+                    all_tags[tag['tag_id']] = {'data': tag, 'day_count': 1}
+        return all_tags
+
+    @classmethod
+    def _generate_md_top_list(cls, raw_data: RAW_DATA_T) -> str:
+        res = []
+        for video in raw_data:
+            line = '1. '
+            url = f'https://www.bilibili.com/video/{video["bvid"]}'
+            line += f'[{video["title"]}]({url})'
+            res.append(line)
+        return '\n'.join(res)
+
+    @classmethod
+    def generate_md_table_row(cls, row: List[Any]) -> str:
+        return f'| {" | ".join(row)} |\n'
+
+    @classmethod
+    def _generate_tag_distribution(cls, raw_tags: RAW_DATA_T) -> str:
+        summary = []
+        for _, tag in raw_tags.items():
+            name = tag['data']['tag_name']
+            count = str(tag['day_count'])
+            summary.append((name, count))
+
+        summary.sort(key=lambda x: int(x[1]), reverse=True)
+
+        summary_header = ['Tag', 'Count']
+        summary_md = ''
+        summary_md += cls.generate_md_table_row(summary_header)
+        summary_md += cls.generate_md_table_row(['---'] * len(summary_header))
+        for row in summary:
+            summary_md += cls.generate_md_table_row(row)
+
+        return summary_md
+
+    @classmethod
+    def _write_md_for_date(
+        cls, 
+        loc: str, 
+        most_popular: RAW_DATA_T, 
+        highest_ranked: RAW_DATA_T, 
+        most_popular_tags: RAW_DATA_T, 
+        highest_ranked_tags: RAW_DATA_T
+    ) -> None:
+        md_str = '# Top List\n'
+        md_str += '## Highest Ranked Videos\n'
+        md_str += cls._generate_md_top_list(highest_ranked)
+        md_str += '\n\n'
+        md_str += '## Most Popular Videos\n'
+        md_str += cls._generate_md_top_list(most_popular)
+
+        md_str += '\n\n'
+        md_str += '# Tag Distribution\n'
+        md_str += '## Highest Ranked Videos\n'
+        md_str += '\n\n'
+        md_str += cls._generate_tag_distribution(highest_ranked_tags)
+        md_str += '\n\n'
+        md_str += '## Most Popular Videos\n'
+        md_str += cls._generate_tag_distribution(most_popular_tags)
+
+        write_md(md_str, path.join(loc, 'README.md'))
+
+    @classmethod
+    def archive_for_today(cls) -> None:
+        loc = path.join(cls.BASE_PATH, cls.LOC, date.today().isoformat())
+        most_popular_data = cls._get_most_popular()
+        most_popular_aids = (video['aid'] for video in most_popular_data)
+        most_popular_tags = cls._get_tags(most_popular_aids)
+        write_raw_data(most_popular_data, path.join(loc, 'Raw', 'most_popular.json'))
+        write_raw_data(most_popular_tags, path.join(loc, 'Tags', 'most_popular.json'))
+
+        highest_ranked_data = cls._get_highest_ranked()
+        highest_ranked_aids = (video['aid'] for video in highest_ranked_data)
+        highest_ranked_tags = cls._get_tags(highest_ranked_aids)
+        write_raw_data(highest_ranked_data, path.join(loc, 'Raw', 'highest_ranked.json'))
+        write_raw_data(highest_ranked_tags, path.join(loc, 'Tags', 'highest_ranked.json'))
+
+        cls._write_md_for_date(loc, most_popular_data, highest_ranked_data, most_popular_tags, highest_ranked_tags)
+
+class GithubAPI(BaseApi):
+    LOC = 'Github'
+    BASE_URL = 'https://github.com'
+    RAW_DATA_T = List[Dict[str, Any]]
+
+    @classmethod
+    def get_trending(cls) -> RAW_DATA_T:
+        soup = cls._get_parsed_html(f"{cls.BASE_URL}/trending")
+        articles = soup.find_all('article', class_='Box-row')
+        trending_repos = []
+        for article in articles:
+            title = re.sub(r'\s+', '', article.find('h2').text)
+            url = f"{cls.BASE_URL}/{article.find('h2').find('a')['href']}"
+
+            description_tag = article.find('p')
+            description = description_tag.text.strip() if description_tag else ''
+
+            language_tag = article.find(attrs={'itemprop': 'programmingLanguage'})
+            language = language_tag.text.strip() if language_tag else ''
+
+            stars_tag = article.find('a', href=re.compile(r'/stargazers$'))
+            stars = stars_tag.text.strip() if stars_tag else ''
+
+            forks_tag = article.find('a', href=re.compile(r'/forks$'))
+            forks = forks_tag.text.strip() if forks_tag else ''
+
+
+            contributors_tag = article.find_all('a', attrs={'data-hovercard-type': 'user'})
+            contributors = []
+            for contributor_tag in contributors_tag:
+                img_tag = contributor_tag.find('img')
+                name = img_tag['alt'].lstrip('@')
+                url = f"{cls.BASE_URL}/{name}"
+                avatar = img_tag['src']
+                contributors.append({
+                    'name': name,
+                    'url': url,
+                    'avatar': avatar,
+                }),
+
+            trending_repos.append({
+                'title': title,
+                'url': url,
+                'description': description,
+                'language': language,
+                'stars': stars,
+                'forks': forks,
+                'contributors': contributors,
+            })
+        return trending_repos
+
+    @classmethod
+    def _write_md_for_date(
+        cls, 
+        loc: str, 
+        trending_repos: RAW_DATA_T, 
+    ) -> None:
+        md_str = '# Trending\n'
+        md_str += '| Repository | Description | Language | Stars | Forks |\n'
+        md_str += '| --- | --- | --- | --- | --- |\n'
+        for repo in trending_repos:
+            md_str += f'| [{repo["title"]}]({repo["url"]}) | {repo["description"]} | {repo["language"]} | {repo["stars"]} | {repo["forks"]} |\n'
+
+        write_md(md_str, path.join(loc, 'README.md'))
+
+    @classmethod
+    def archive_for_today(cls) -> None:
+        loc = path.join(cls.BASE_PATH, cls.LOC, date.today().isoformat())
+        trending_repos = cls.get_trending()
+        write_raw_data(trending_repos, path.join(loc, 'trending.json'))
+        cls._write_md_for_date(loc, trending_repos)
+
+if __name__ == '__main__':
+    result = GithubAPI.get_trending()
+    print(result[0])
--- writer.py
+++ writer.py
@@ -4,17 +4,13 @@ import json
 from os import path, makedirs
 from functools import partial
 
-from api import BilibiliApi
-
-
 def write_md(md_str: str, filepath:str) -> None:
     encoder = lambda x: x
     _write(md_str, encoder, filepath)
 
 
-def write_raw_data(raw_videos: BilibiliApi.RAW_DATA_T, filepath:str) -> None:
+def write_raw_data(raw_videos: Any, filepath:str) -> None:
     encoder = partial(json.dumps, ensure_ascii=False)
-
     _write(raw_videos, encoder, filepath)
 
 
